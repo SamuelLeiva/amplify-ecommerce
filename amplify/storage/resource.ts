be3d5b4fde76c73b48bdd65c64f46934aab153ad
SamuelLeiva <leiva20152@gmail.com>
2025-10-28 12:45:24 -0500

    New product images bucket added.

---
+++ amplify/storage/resource.ts
@@ -0,0 +1,12 @@
+import { defineStorage } from '@aws-amplify/backend';
+
+export const storage = defineStorage({
+  name: 'marketplaceStorage',
+
+  access: (allow) => ({
+    "products-images/*": [
+        allow.authenticated.to(["read", "write"]),
+        allow.guest.to(["read"])
+    ]
+  })
+});
